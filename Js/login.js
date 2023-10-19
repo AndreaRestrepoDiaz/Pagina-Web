5b7accc2af2c2bfd5930be584583eecb9d70675c
AndreaRestrepoDiaz <Andrea.restrepo1@utp.edu.co>
2023-10-19 15:29:55 -0500

    Agregar formulario

---
+++ Js/login.js
@@ -0,0 +1,10 @@
+
+document.getElementById("enviarBoton").addEventListener('click', function(){
+        // Obtener los valores de los campos de entrada
+        var nombre = document.getElementById("nombre").value;
+        var correo = document.getElementById("correo").value;
+    
+        // Mostrar los valores en la consola
+        console.log("Nombre: " + nombre);
+        console.log("Correo electrónico: " + correo);
+})
