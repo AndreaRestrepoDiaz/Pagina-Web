e9ab9be4676b74212ea0ac913ad87c72b02fe7c4
AndreaRestrepoDiaz <Andrea.restrepo1@utp.edu.co>
2023-11-18 12:28:47 -0500

    Modificaciones

---
+++ Pagina-Web - copia/assets/js/login.js
@@ -0,0 +1,7 @@
+document.getElementById("botton_login").addEventListener('click',function(){
+    var nombre = document.getElementById("input_nombre").value;
+    var contrasenia = document.getElementById("input_contrasenia").value;
+
+    console.log(nombre)
+    console.log(contrasenia)
+});
